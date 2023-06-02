f5a8f24ca3bccc3d84c1fafcdbaa8676dcce60eb
Nicolino07 <97976167+Nicolino07@users.noreply.github.com>
2023-06-01 21:46:42 -0300

    Add files via upload
    Contiene el modulo de menú principal, calculadora clásica, calculadora de fracciones y calculadora de conversiones.

---
+++ Calculadora/Calculadora_Clasica_Yeremi.py
@@ -0,0 +1,44 @@
+
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("No se puede dividir entre cero.")
+        return None
+
+# Solicitar el primer valor al usuario
+
+valor1 = float(input("Ingrese el primer valor:\n "))
+resultado = valor1
+
+while True:
+    operador = input("Ingrese el operador (+, -, *, /) o '=' para obtener el resultado: \n")
+
+    if operador == '=':
+        break
+
+    valor2 = float(input("Ingrese el siguiente valor: \n"))
+
+    if operador == '+':
+        resultado = sumar(resultado, valor2)
+    elif operador == '-':
+        resultado = restar(resultado, valor2)
+    elif operador == '*':
+        resultado = multiplicar(resultado, valor2)
+    elif operador == '/':
+        resultado = dividir(resultado, valor2)
+    else:
+        print("Operador inválido.")
+
+# Mostrar el resultado final
+print("El resultado es:\n", resultado)
